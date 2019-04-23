2706d456476924f87a1860053f5c8ecd3fca19f6
sidratariq <sidratariq48@gmail.com>
2019-04-23 01:28:15 +0000

    playground styling done

--- src/store/modules/rect/state.js
+++ src/store/modules/rect/state.js
@@ -1,27 +1,6 @@
 export default {
     rects: [
-        // {
-        //     width: 101,
-        //     height: 27,
-        //     top: 10,
-        //     left: 10,
-        //     draggable: true,
-        //     resizable: true,  
-        //     minw: 20,
-        //     minh: 20,
-        //     axis: "both",
-        //     parentLim: true,
-        //     snapToGrid: false,
-        //     aspectRatio: false,
-        //     zIndex: 1,
-        //     color:"#f7b994  no-repeat  ",
-        //     active: false,
-        //     text: args,
-        //     recipient:this.recipientid,
-        //     recipientname: this.recipientname,
-        //     contractid:this.Contractid
-    
-        //   } 
+      
 
     ]
 };
