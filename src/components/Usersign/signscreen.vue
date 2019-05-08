e9a3de775337cb2db5059f35c5d27cbf6210c6f0
sidratariq <sidratariq48@gmail.com>
2019-05-08 05:06:54 +0000

    removed junk folder

--- src/components/Usersign/signscreen.vue
+++ src/components/Usersign/signscreen.vue
@@ -8,7 +8,7 @@
         <div class="col-12 col-md-12" style="padding-left:0px">
           <div class="row">
             <div class="col-2">
-              <div class="col-3"></div>
+              <div class="col-3">was not adding because i was in junk folder</div>
             </div>
           </div>
         </div>
